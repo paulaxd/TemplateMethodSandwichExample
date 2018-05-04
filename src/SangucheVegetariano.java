3885cb984db466fdd307b62b1b2a9a8e4e19912a
Paula <m.paula.santamaria@gmail.com>
2018-05-04 11:56:02 -0300

    Add comment

--- src/SangucheVegetariano.java
+++ src/SangucheVegetariano.java
@@ -6,6 +6,7 @@ public class SangucheVegetariano extends Sanguche {
 		System.out.println("Agregando: Vegetales, huevo, queso");
 	}
 	
+	// En este caso sobreescribo elegirPan() para reemplazar la implementación del Hook Method original.
 	@Override
 	public void elegirPan() {
 		System.out.println("Eligiendo pan: Negro");
